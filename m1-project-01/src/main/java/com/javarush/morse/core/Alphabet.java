497e255409058325c3d2e25feaf0b1d6f44af8ee
”Aleksey <lexa-teenz22@ya.ru>
2025-12-08 01:11:31 +0300

    Добавить Алфвав Алфавит

--- m1-project-01/src/main/java/com/javarush/morse/core/Alphabet.java
+++ m1-project-01/src/main/java/com/javarush/morse/core/Alphabet.java
@@ -19,17 +19,66 @@ public class Alphabet {
         // - заполнить русские буквы
         textToMorse.put('А', ".-");
         textToMorse.put('Б', "-...");
-        // todo продолжить от А до Я
+        textToMorse.put('В', ".--");
+        textToMorse.put('Г', "--.");
+        textToMorse.put('Д', "-..");
+        textToMorse.put('Е', ".");
+        textToMorse.put('Ё', ".");
+        textToMorse.put('Ж', "...-");
+        textToMorse.put('З', "--..");
+        textToMorse.put('И', "..");
+        textToMorse.put('Й', ".---");
+        textToMorse.put('К', "-.-");
+        textToMorse.put('Л', ".-..");
+        textToMorse.put('М', "--");
+        textToMorse.put('Н', "-.");
+        textToMorse.put('О', "---");
+        textToMorse.put('П', ".--");
+        textToMorse.put('Р', ".-.");
+        textToMorse.put('С', "...");
+        textToMorse.put('Т', "-");
+        textToMorse.put('У', "..-");
+        textToMorse.put('Ф', "..-.");
+        textToMorse.put('Х', "....");
+        textToMorse.put('Ц', "-.-.");
+        textToMorse.put('Ч', "---.");
+        textToMorse.put('Ш', "----");
+        textToMorse.put('Щ', "--.-");
+        textToMorse.put('Ъ', "--.--");
+        textToMorse.put('Ы', "-.--");
+        textToMorse.put('Ь', "-..-");
+        textToMorse.put('Э', "..-..");
+        textToMorse.put('Ю', "..--");
         textToMorse.put('Я', ".-.-");
 
         // - заполнить цифры (*)
-        textToMorse.put('0', "-----");
-        // todo продолжить от 1 до 8
+        textToMorse.put('1', ".----");
+        textToMorse.put('2', "..---");
+        textToMorse.put('3', "...--");
+        textToMorse.put('4', "....-");
+        textToMorse.put('5', ".....");
+        textToMorse.put('6', "-....");
+        textToMorse.put('7', "--...");
+        textToMorse.put('8', "---..");
         textToMorse.put('9', "----.");
+        textToMorse.put('0', "-----");
 
         // - заполнить пунктуацию (*)
-        textToMorse.put('.', "......");
-        // todo заполнить остальные знаки пунктуации
+        textToMorse.put('.', ".-.-.-"); // Точка
+        textToMorse.put(',', "--..--"); // Запятая
+        textToMorse.put('?', "..--.."); // Вопросительный знак
+        textToMorse.put('!', "-.-.--"); // Восклицательный знак
+        textToMorse.put(':', "---..."); // Двоеточие
+        textToMorse.put(';', "-.-.-."); // Точка с запятой
+        textToMorse.put('(', "-.--."); // Открывающая скобка
+        textToMorse.put(')', "-.--.-"); // Закрывающая скобка
+        textToMorse.put('"', ".-..-."); // Кавычки
+        textToMorse.put('\'', ".----."); // Апостроф
+        textToMorse.put('-', "-....-"); // Дефис / Минус
+        textToMorse.put('/', "-..-."); // Косая черта
+        textToMorse.put('=', "-...-"); // Знак равенства (также используется как разделитель)
+        textToMorse.put('+', ".-.-."); // Знак плюс
+        textToMorse.put('@', ".--.-."); // Символ @ (коммерческое at)
         textToMorse.put(' ', "/");
 
         // - создать обратное отображение
